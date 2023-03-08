6ba79dccd1ee963b39b741188fa9f388e26716b8
caizicharles <zc348@cam.ac.uk>
2023-03-08 22:50:12 +0000

    commit

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
+import numpy as np
+import reply_data_cleaning as rdc
 
+
+
+def grad_des_single(eta):
+    return None
+
+def update(iter_num = 20):
+
+
+    return None
+
+def main():
+    input = np.array(rdc.input())
+
+
+
+    return None
+
+#main()
