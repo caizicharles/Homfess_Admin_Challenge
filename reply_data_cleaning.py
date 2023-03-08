30845c25247040bb8f776d8a1d6f113c307d5070
yanndivet <98603915+yanndivet@users.noreply.github.com>
2023-03-08 00:21:56 +0000

    Add files via upload

---
+++ reply_data_cleaning.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar  7 23:23:56 2023
+
+@author: yannd
+"""
+
+f = open('example.txt', 'r')
+lines = f.readlines()
+data = []
+
+# Read and clean data
+for line in lines:
+    liste = line.split()
+    int_liste = [int(idx) for idx in liste]
+    data.append(int_liste)
+    
+print(data)
+
+a = 1
