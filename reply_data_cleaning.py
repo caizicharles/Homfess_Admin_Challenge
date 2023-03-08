8675cabf18e6a45f9211fff9678654e3b2b76f0d
caizicharles <zc348@cam.ac.uk>
2023-03-08 22:49:32 +0000

    structure

--- reply_data_cleaning.py
+++ reply_data_cleaning.py
@@ -5,14 +5,20 @@ Created on Tue Mar  7 23:23:56 2023
 @author: yannd
 """
 
-f = open('example.txt', 'r')
-lines = f.readlines()
-data = []
+def input():
+    f = open('example.txt', 'r')
+    lines = f.readlines()
+    data = []
 
-# Read and clean data
-for line in lines:
-    liste = line.split()
-    int_liste = [int(idx) for idx in liste]
-    data.append(int_liste)
+    # Read and clean data
+    for line in lines:
+        liste = line.split()
+        int_liste = [int(idx) for idx in liste]
+        data.append(int_liste)
+        #print(data)
+
+    return data
+
+def output():
     
-print(data)
+    return None
