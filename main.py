e7bf1dcc0ed27fd3a417d217e941afa9408cb330
caizicharles <zc348@cam.ac.uk>
2023-03-09 16:58:23 +0000

    /

--- main.py
+++ main.py
@@ -1,18 +1,26 @@
 import numpy as np
 import reply_data_cleaning as rdc
 
-
-
-def gradient_descent_single_update(eta):
+def pre_processing():
     return None
 
-def gradient_descent(iter_num = 20):
-
-
-    return None
 
 def main():
     input = np.array(rdc.input())
+
+    R = input[0][0]
+    C = input[0][1]
+    S_num = input[0][2]
+    S_length = input[1]
+    iter_num = 20
+    #eta = 
+
+    Snakes = []
+
+    for i in range(0, S_num):
+        for j in range(0, S_length[i]):
+            Snakes.append([0,0])
+    Snakes = np.array(Snakes)
 
 
 
