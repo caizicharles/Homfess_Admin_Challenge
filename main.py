f7d6f114c74f8f3afc42bee468e4ab8b9bcaed1b
caizicharles <zc348@cam.ac.uk>
2023-03-09 17:36:25 +0000

    /

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
 import numpy as np
 import reply_data_cleaning as rdc
 
-def pre_processing():
-    return None
+def pre_processing(S_num, S_length):
+    Snakes_list = []
+
+    for i in range(0, S_num):
+        s = np.array([[2,2]])
+        for j in range(0, S_length[i]-1):
+            s = np.concatenate((s, np.array([[0,0]])))
+        each_snake = np.array(s)
+        Snakes_list.append(each_snake)
+    
+    return np.array(Snakes_list)
 
 
 def main():
@@ -11,18 +20,15 @@ def main():
     R = input[0][0]
     C = input[0][1]
     grid = np.array(input[2:])
+
     S_num = input[0][2]
     S_length = input[1]
+    Snakes = pre_processing(S_num, S_length)
+    
     iter_num = 20
     #eta = 
 
-    Snakes = []
-
-    for i in range(0, S_num):
-        for j in range(0, S_length[i]):
-            Snakes.append([0,0])
-    Snakes = np.array(Snakes)
-
+    
 
 
     return None
