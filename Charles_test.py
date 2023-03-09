614753f3033c324a990fecef8057fe5405cf202c
caizicharles <zc348@cam.ac.uk>
2023-03-09 17:09:15 +0000

    /

--- Charles_test.py
+++ Charles_test.py
@@ -1,17 +1,20 @@
 import numpy as np
 
-'''Snakes = 
+'''Snakes = np.array([[1,1]])
 S_num = 2
 S_length = [2,3]
 
-for i in range(0, (S_num-1)):
+for i in range(0, S_num):
     for j in range(0, S_length[i]-1):
         s = np.array([[0,0]])
         s = np.concatenate((s, np.array([[0,0]])))
-    Snakes.append(np.array(s))
-print(Snakes)'''
+    Snakes = np.concatenate((Snakes, np.array(s)))
+print(Snakes)
 
-a = np.array([[0,0]])
-print(a)
+a = np.array([[0,0],[0,0]])
 b = np.array([[0,0]])
-print(np.concatenate((a,b)))
+a = np.concatenate((a,b))
+print(a)'''
+
+a = [[0],[1],[2]]
+print(np.array(a[1:]))
